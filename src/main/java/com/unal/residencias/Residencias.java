dd4106d482e69986975323c603f4a9f69ed4d050
JDavidCS <jdcorreasierra@gmail.com>
2025-11-22 11:43:54 -0500

    proyecto inicial

---
+++ src/main/java/com/unal/residencias/Residencias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unal.residencias;
+
+/**
+ *
+ * @author jdcor
+ */
+public class Residencias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
