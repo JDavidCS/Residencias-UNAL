e6eb1555e56d711e20d1633637989f285b2128d7
ketrin12345 <kealdanam@unal.edu.co>
2025-11-23 15:02:15 -0500

    Crear Heap

---
+++ src/main/java/com/unal/residencias/MinHeap.java
@@ -0,0 +1,12 @@
+package com.unal.residencias;
+
+import java.util.ArrayList;
+
+public class MinHeapEstudiantes {
+    private ArrayList<Estudiante> heap;
+
+    public MinHeapEstudiantes() {
+        heap = new ArrayList<>();
+    }
+
+}
