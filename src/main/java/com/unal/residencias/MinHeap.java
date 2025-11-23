bdba0d62848278301988071a906cf1da6067af35
ketrin12345 <kealdanam@unal.edu.co>
2025-11-23 16:17:12 -0500

    Añadi metodos
    Añadido swap, siftUp, siftDown y insertar

--- src/main/java/com/unal/residencias/MinHeap.java
+++ src/main/java/com/unal/residencias/MinHeap.java
@@ -2,7 +2,7 @@ gdspackage com.unal.residencias;
 
 import java.util.ArrayList;
 
-public class MinHeapEstudiantes {
+public class MinHeap{
     private ArrayList<Estudiante> heap;
 
     public MinHeapEstudiantes() {
@@ -18,7 +18,41 @@ public class MinHeapEstudiantes {
     private int rightChild(int i){ 
       return 2 * i + 2; 
     }
+    
+    private void swap(int i, int j) {
+        Estudiante temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }    
 
-    fdsfdscxzcfsdfxczxcd
+    private void siftUp(int i) {
+        while (i > 0 && heap.get(parent(i)).getPuntaje() > heap.get(i).getPuntaje()) {
+            swap(i, parent(i));
+            i = parent(i);
+        }
+    }
+
+    private void siftDown(int i) {
+        int min = i;
+
+        int left = leftChild(i);
+        if (left < heap.size() && heap.get(left).getPuntaje() < heap.get(min).getPuntaje()){
+            min = left;
+        }
+        int right = rightChild(i);
+        if (right < heap.size() && heap.get(right).getPuntaje() < heap.get(min).getPuntaje()){
+            min = right;
+        }
+        if (min != i){
+            swap(i, min);
+            siftDown(min);
+        }
+    }
+
+    public void insertar(Estudiante est){
+        heap.add(est);
+        siftUp(heap.size() - 1);
+    }
+
     
 }
